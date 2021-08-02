bfc1fc690a1e128010cf1162b42f344a893fc956
Vinay Pilaniya <82079006+Pilaniya1@users.noreply.github.com>
2021-08-02 20:42:40 +0530

    Add files via upload
    ->Create array of character type.
    ->and assign the character values on the array position .

---
+++ array1character.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    char a[5];
+    char i,b,c,d,e,f;
+    a[0]= 'b';
+    a[1]= 'c';
+    a[2]= 'd';
+    a[3]= 'e';
+    a[4]= 'f';
+
+    cout<<a[0];
+    cout<<a[1];
+    cout<<a[2];
+    cout<<a[3];
+    cout<<a[4];
+    
+   
+}
+    
+
+
+
